f02d816f0c9fcb4be91b2960ba63f22e6e45887e
ilhamraff <ifadhilah261@gmail.com>
2025-09-30 21:20:18 +0700

    feat: enhance authentication views and add subscription check middleware; update registration and login forms

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,15 +31,32 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'email' => [
+                'required',
+                'string',
+                'lowercase',
+                'email',
+                'max:255',
+                'unique:' . User::class,
+            ],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'occupation' => ['required', 'string', 'max:255'],
+            'photo' => ['required', 'image', 'mimes:png,jpg,jpeg'],
         ]);
+
+        if ($request->hasFile('photo')) {
+            $photoPath = $request->file('photo')->store('photos', 'public');
+        }
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'occupation' => $request->occupation,
+            'photo' => $photoPath,
         ]);
+
+        $user->assignRole('student');
 
         event(new Registered($user));
 
